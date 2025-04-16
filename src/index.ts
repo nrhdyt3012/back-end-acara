863eec3afed5d3e1238eeb2b99c4748d95d4d52e
nrhdyt3012 <dayats354313@gmail.com>
2025-04-16 11:42:01 +0700

    Validation API Register

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import  express  from "express";
+import bodyParser from "body-parser";
 import router from "./routes/api";
 
 const app = express();
+
+app.use(bodyParser.json());
 
 const PORT = 3000;
 
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,9 @@
 import  express from "express";
 
+import authController from '../controllers/auth.controller'
+
 const router = express.Router();
 
+router.post('/auth/register', authController.register)
 
 export default router;
